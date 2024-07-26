91743e009f8cd7ff075a847fd0421796ef77fc25
EmaOlmos <eduardoolmosdionisio@gmail.com>
2024-07-26 15:52:09 -0600

    last commit (I think, lol)

---
+++ js.js
@@ -0,0 +1,49 @@
+const btn = document.getElementById("submit-btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const input = document.getElementById("number-input");
+  const intInput = parseInt(input.value);
+  const result = document.getElementById("res");
+
+  result.classList.remove("hidden");
+  input.value = "";
+
+  if (!intInput) {
+    result.textContent = "Hey, that's not a valid input :]";
+    return;
+  }
+  if (intInput > 3999) {
+    result.textContent = "Please, write a number less than or equal to 3999.";
+    return;
+  }
+
+  result.textContent = convertToRoman(intInput);
+});
+
+const convertToRoman = (num) => {
+  const roman = [
+      "M",
+      "CM",
+      "D",
+      "CD",
+      "C",
+      "XC",
+      "L",
+      "XL",
+      "X",
+      "IX",
+      "V",
+      "IV",
+      "I",
+    ],
+    value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let res = "";
+
+  for (let i = 0; num > 0; i++) {
+    while (num >= value[i]) {
+      res += roman[i];
+      num -= value[i];
+    }
+  }
+  return res;
+};
